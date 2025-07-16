e9396a89fc28d6db7977396c44f73bb8057d036a
Nick Budzban <nichbud@outlook.com>
2025-07-16 17:46:11 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GameSelection } from './components/game-selection/game-selection';
+import { LiveGame } from './components/live-game/live-game';
+import { ModelPlayersComponent } from './model-players/model-players';
+import { GameProbabilityPageComponent } from './game-probability-page/game-probability-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameSelection },
+  { path: 'game/:gamePk', component: LiveGame },
+  { path: 'model-players/:gamePk/:season', component: ModelPlayersComponent },
+  { path: 'game-probability/:playerId/:season', component: GameProbabilityPageComponent },
+];
